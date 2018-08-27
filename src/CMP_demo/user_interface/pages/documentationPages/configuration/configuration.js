15d55d12bdbcf548b9d18955800d56cf9c21d23e
Pablo Vega Uceta <pablo.vega@scmspain.com>
2018-08-27 13:21:02 +0200

    delete non necessary comments

--- src/CMP_demo/user_interface/pages/documentationPages/configuration/configuration.js
+++ src/CMP_demo/user_interface/pages/documentationPages/configuration/configuration.js
@@ -1,20 +1,3 @@
-// import React, {Component} from 'react'
-// import marked from 'marked'
-// import {configurationText} from './configurationText.js'
-
-// class configuration extends Component {
-//   componentDidMount() {
-//     const rawMarkup = marked(configurationText, {sanitize: true})
-//     document.getElementById('configuration').innerHTML = rawMarkup
-//   }
-
-//   render() {
-//     return <div id="configuration" />
-//   }
-// }
-
-// export default configuration
-
 import React, {Component} from 'react'
 import ServiceMarkdown from '@s-ui/react-services-markdown'
 

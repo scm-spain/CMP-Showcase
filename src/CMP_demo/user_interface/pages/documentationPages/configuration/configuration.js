c6a5772d004babe47df5a4183e45c38bc87b5c97
Pablo Vega Uceta <pablo.vega@scmspain.com>
2018-08-27 13:18:39 +0200

    Use SUI serviceMarkdown component to show the documentation

--- src/CMP_demo/user_interface/pages/documentationPages/configuration/configuration.js
+++ src/CMP_demo/user_interface/pages/documentationPages/configuration/configuration.js
@@ -1,15 +1,28 @@
+// import React, {Component} from 'react'
+// import marked from 'marked'
+// import {configurationText} from './configurationText.js'
+
+// class configuration extends Component {
+//   componentDidMount() {
+//     const rawMarkup = marked(configurationText, {sanitize: true})
+//     document.getElementById('configuration').innerHTML = rawMarkup
+//   }
+
+//   render() {
+//     return <div id="configuration" />
+//   }
+// }
+
+// export default configuration
+
 import React, {Component} from 'react'
-import marked from 'marked'
-import {configurationText} from './configurationText.js'
+import ServiceMarkdown from '@s-ui/react-services-markdown'
 
 class configuration extends Component {
-  componentDidMount() {
-    const rawMarkup = marked(configurationText, {sanitize: true})
-    document.getElementById('configuration').innerHTML = rawMarkup
-  }
-
   render() {
-    return <div id="configuration" />
+    return (
+      <ServiceMarkdown src="src/CMP_demo/user_interface/pages/documentationPages/configuration/configuration.md" />
+    )
   }
 }
 

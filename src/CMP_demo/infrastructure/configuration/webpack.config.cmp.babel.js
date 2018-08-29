9d6aaeb6818d71942511dcfcd31aba8958d95723
rafael-marquez <rafael.marquez@scmspain.com>
2018-08-29 15:14:14 +0200

    Refactor webpack configuration + Remove unused Package.json tasks.

---
+++ src/CMP_demo/infrastructure/configuration/webpack.config.cmp.babel.js
@@ -0,0 +1,42 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path from 'path'
+import CleanWebpackPlugin from 'clean-webpack-plugin'
+
+const OUTPUT_DIST_FOLDER = 'dist'
+const ENTRY_PATH = './src/CMP_Demo/user_interface/index.js'
+const TEMPLATE_PATH = './src/CMP_Demo/user_interface/index.html'
+
+let webpackConfig = {
+  entry: path.resolve(ENTRY_PATH),
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(TEMPLATE_PATH)
+    }),
+    new CleanWebpackPlugin([OUTPUT_DIST_FOLDER], {
+      verbose: true,
+      root: process.cwd()
+    })
+  ]
+}
+
+module.exports = (env, argv) => {
+  return webpackConfig
+}

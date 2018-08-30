adc24b7dae235c7e02ba5e1947e8c2219683483a
Pablo Vega Uceta <pablovegau@gmail.com>
2018-08-30 13:33:45 +0200

    Feature/pass context to init method (#11)
    * change the method to switch on Boros-cmp
    
    * import Boros-cmp via NPM

--- src/CMP_demo/user_interface/index.js
+++ src/CMP_demo/user_interface/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
-
 import App from './components/App'
-
 import './index.scss'
+import boroscmp from '@schibstedspain/boros-cmp'
+
+boroscmp.init()
 
 ReactDOM.render(
   <BrowserRouter>

8c8a44cd8867134c83854ceecb9221b9a5f74e16
Pablo Vega Uceta <pablo.vega@scmspain.com>
2018-08-30 12:42:31 +0200

    change the method to switch on Boros-cmp

--- src/CMP_demo/user_interface/components/UI/Nav/Nav.js
+++ src/CMP_demo/user_interface/components/UI/Nav/Nav.js
@@ -7,7 +7,7 @@ const nav = () => (
   <div>
     <div className={`${baseClass}`}>
       <NavLink to="/" exact>
-        BorosJS
+        Boros
       </NavLink>
       <div className={`${baseClass}-subtitle`}>Documentation</div>
       <NavLink to="#">Lorem Ipsum</NavLink>

67f7c54a1f7cb640b7ebb51f4b5cdd0d68ab8bb4
Pablo Vega Uceta <pablovegau@gmail.com>
2018-08-30 12:47:43 +0200

    Revert "add css file to style markdowns (#8)"
    This reverts commit 96a9ace705ca0f4762c7dd59e50e6743e5b9ecb0.

--- src/CMP_demo/user_interface/pages/documentationPages/configuration/configuration.js
+++ src/CMP_demo/user_interface/pages/documentationPages/configuration/configuration.js
@@ -4,9 +4,7 @@ import ServiceMarkdown from '@s-ui/react-services-markdown'
 class configuration extends Component {
   render() {
     return (
-      <article className="markdown-body">
-        <ServiceMarkdown src="src/CMP_demo/user_interface/pages/documentationPages/configuration/configuration.md" />
-      </article>
+      <ServiceMarkdown src="src/CMP_demo/user_interface/pages/documentationPages/configuration/configuration.md" />
     )
   }
 }
